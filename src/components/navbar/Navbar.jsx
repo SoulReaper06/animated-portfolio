2bfa5af7e4d1064f8c2e4168e0026a5c8ba4cfae
SoulReaper06 <johnatj33@gmail.com>
2023-10-23 08:50:16 +0530

    working on Service Section

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,7 +1,7 @@
 import "./navbar.scss"
 
 import { motion } from "framer-motion"
-import Sidebar from "./sidebar/Sidebar"
+import Sidebar from "../sidebar/Sidebar"
 
 const Navbar = () => {
   return (
