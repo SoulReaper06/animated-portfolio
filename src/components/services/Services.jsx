3186eccb9ca954ac61a78b445ae1e1acea03649f
SoulReaper06 <johnatj33@gmail.com>
2023-10-24 18:26:09 +0530

    Project Section Updated

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,72 +1,94 @@
-import React from 'react'
+import React, { useRef } from 'react'
 
-import { motion } from "framer-motion"
+import { useInView, motion } from "framer-motion"
 
 import './services.scss';
 
+const variants = {
+    initial: {
+        x:-500,
+        y:100,
+        opacity: 0,
+    },
+    animate: {
+        x:0,
+        opacity:1,
+        y:0,
+        transition:{
+            duration:1,
+            staggerChildren: 0.1,
+        }
+    }
+}
+
 const Services = () => {
+
+    const ref = useRef()
+
+    const isInView = useInView(ref,{margin:"-100px"})
+
   return (
-    <motion.div className='services'>
-        <motion.div className="textContainer">
+    <motion.div className='services'
+                variants={variants} 
+                initial="initial" 
+                ref={ref}
+                animate={isInView && "animate"}>
+        <motion.div className="textContainer" variants={variants}>
             <p>I focus on helpin you brand grow <br/> 
                 and move forward</p>
             <hr />
         </motion.div>
-        <motion.div className="titleContainer">
+        <motion.div className="titleContainer" variants={variants}>
             <div className="title">
                 <img src="/people.webp" alt="" />
                 <h1>
-                    <b>Unique</b> Ideas
+                    <motion.b whileHover={{color:"orange"}}>Unique</motion.b> Ideas
                 </h1>
             </div>
             <div className="title">
                 <h1>
-                    <b>For Your</b> Business
+                    <motion.b whileHover={{color:"orange"}}>For Your</motion.b> Business
                 </h1>
                 <button>WHAT WE DO ?</button>
             </div>
         </motion.div>
-        <motion.div className="listContainer">
-            <div className="box">
+        <motion.div className="listContainer" variants={variants}>
+            <motion.div className="box" whileHover={{background:"lightgray",color:"black"}}>
+                <h2>
+                    Branding
+                </h2>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Omnis, labore architecto delectus rem ea, 
+                    temporibus deserunt perspiciatis accusamus laudantium cupiditate debitis corrupti?
+                </p>
+                <button>Go</button>
+            </motion.div>
+            <motion.div className="box" whileHover={{background:"lightgray",color:"black"}}>
                 <h2>
                     Branding
                 </h2>
                 <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Omnis, labore architecto delectus rem ea, 
                     temporibus deserunt perspiciatis accusamus laudantium cupiditate debitis corrupti? 
-                    Porro, dolore! Deleniti sint consequuntur ab sunt laboriosam?
                 </p>
                 <button>Go</button>
-            </div>
-            <div className="box">
+            </motion.div>
+            <motion.div className="box" whileHover={{background:"lightgray",color:"black"}}>
                 <h2>
                     Branding
                 </h2>
                 <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Omnis, labore architecto delectus rem ea, 
                     temporibus deserunt perspiciatis accusamus laudantium cupiditate debitis corrupti? 
-                    Porro, dolore! Deleniti sint consequuntur ab sunt laboriosam?
                 </p>
                 <button>Go</button>
-            </div>
-            <div className="box">
+            </motion.div>
+            <motion.div className="box" whileHover={{background:"lightgray",color:"black"}}>
                 <h2>
                     Branding
                 </h2>
                 <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Omnis, labore architecto delectus rem ea, 
                     temporibus deserunt perspiciatis accusamus laudantium cupiditate debitis corrupti? 
-                    Porro, dolore! Deleniti sint consequuntur ab sunt laboriosam?
                 </p>
                 <button>Go</button>
-            </div>
-            <div className="box">
-                <h2>
-                    Branding
-                </h2>
-                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Omnis, labore architecto delectus rem ea, 
-                    temporibus deserunt perspiciatis accusamus laudantium cupiditate debitis corrupti? 
-                    Porro, dolore! Deleniti sint consequuntur ab sunt laboriosam?
-                </p>
-                <button>Go</button>
-            </div>
+            </motion.div>
         </motion.div>
     </motion.div>
   )
